86ca8e2d270069a1171b6c0ae49849a5be5f09b6
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-15 23:57:21 +0100

    typescript-eslint imported properly

--- part9b/eslint.config.mjs
+++ part9b/eslint.config.mjs
@@ -1,5 +1,5 @@
 import eslint from '@eslint/js'
-import tseslint from '@typescript-eslint'
+import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
 
 export default tseslint.config({
