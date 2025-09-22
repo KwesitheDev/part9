b8a0cb8f784851e37796d823730164260251c7f4
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-22 03:15:29 +0100

    fixed getDiagnosis-properly return ed diagnosis

--- patientor-backend/src/services/diagnosisServices.ts
+++ patientor-backend/src/services/diagnosisServices.ts
@@ -2,7 +2,9 @@ import diagnosisData from '../data/diagnoses';
 import { Diagnosis } from '../types';
 
 const diagnosis: Diagnosis[] = diagnosisData;
-const getDiagnosis = (): Diagnosis[]=> diagnosis ;
+const getDiagnosis = (): Diagnosis[] => {
+    return diagnosis;;
+};
 
 export default {
     getDiagnosis
