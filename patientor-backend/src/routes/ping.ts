a0ce0415d3bf6a2fc27a19c85e1d0df1a91bd8d3
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-22 02:58:36 +0100

    removed ping endpoint

--- patientor-backend/src/routes/ping.ts
+++ patientor-backend/src/routes/ping.ts
@@ -2,7 +2,7 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/ping', (_req, res) => {
+router.get('/', (_req, res) => {
     res.send('Someone pinged here');
     console.log('pong');
 });
