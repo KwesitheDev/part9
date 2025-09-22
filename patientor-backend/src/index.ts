272694634362c0cae0131ecb35137c3a42db4173
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-22 02:19:40 +0100

    imported ping route

--- patientor-backend/src/index.ts
+++ patientor-backend/src/index.ts
@@ -1,13 +1,13 @@
 import express from 'express';
+import pingRouter from './routes/ping';
 import cors from 'cors';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/api/ping', (_req, res) => {
-    res.send('pong');
-});
+//ping route
+app.use('/api/ping', pingRouter);
 
 const PORT = 3001;
 
