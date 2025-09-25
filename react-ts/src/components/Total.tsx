3a6af6eeca81e1cbb2f3fa01d2a7bde94e670d22
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-25 10:08:04 +0100

    Total component

---
+++ react-ts/src/components/Total.tsx
@@ -0,0 +1,11 @@
+type TotalProps = {
+    total: number;
+}
+
+const Total = (props: TotalProps) => {
+    return (
+        <p>Number of exercises {props.total}</p>
+    )
+}
+
+export default Total;
