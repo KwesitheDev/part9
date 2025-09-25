b311845fa2c2b56f791abd06aa2842afb8d4b9d8
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-25 05:19:37 +0100

    added validation to post endpoint

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,6 +1,7 @@
 import  express from "express";
 import patientService from "../services/patientServices";
-import { NewPatient } from "../types";
+//import { NewPatient } from "../types";
+import { toNewPatient } from "../utils";
 
 const router = express.Router();
 
@@ -8,11 +9,18 @@ router.get('/', (_req, res) => {
     res.json(patientService.getPatients());
 });
 
-router.post('/',(_req, res) => {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const newPatient: NewPatient = _req.body;
+router.post('/', (req, res) => {
+  try {
+    const newPatient = toNewPatient(req.body);
     const addedPatient = patientService.addPatient(newPatient);
     res.json(addedPatient);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  }
 });
 
 
