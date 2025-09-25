6cab847b9111e821a7478ed7ed475838f491a8c0
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-25 05:01:15 +0100

    added POST endpoint

--- patientor-backend/src/routes/patients.ts
+++ patientor-backend/src/routes/patients.ts
@@ -1,10 +1,18 @@
 import  express from "express";
 import patientService from "../services/patientServices";
+import { NewPatient } from "../types";
 
 const router = express.Router();
 
 router.get('/', (_req, res) => {
     res.json(patientService.getPatients());
+});
+
+router.post('/',(_req, res) => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const newPatient: NewPatient = _req.body;
+    const addedPatient = patientService.addPatient(newPatient);
+    res.json(addedPatient);
 });
 
 
