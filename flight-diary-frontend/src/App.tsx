9ee228fcc6b028d7b89839f7ae96d91520b44a26
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-26 07:46:09 +0100

    get diaries and display

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import type { DiaryEntry } from "./types";
+import * as diaryService from './diaryService';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+ const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+  useEffect(() => {
+    diaryService.getAll().then(setDiaries);
+  },[])
+
+
+  return (<div>
+    <h1>Flight Diaries</h1>
+    {diaries.map(d => (
+      <div key={d.id}>
+        <h3>{d.date}</h3>
+        <p>Visibility: {d.visibility}</p>
+        <p>Weather: {d.weather}</p>
+        {d.comment && <p><i>{d.comment}</i></p>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    ))}
+  </div>
+  );
+
 }
 
-export default App
+export default App;
