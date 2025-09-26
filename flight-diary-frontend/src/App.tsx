18d6bc626a36db33cbf2fd3cf4b288458a8ee52a
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-26 08:03:37 +0100

    addDairy, form added

--- flight-diary-frontend/src/App.tsx
+++ flight-diary-frontend/src/App.tsx
@@ -4,15 +4,39 @@ import * as diaryService from './diaryService';
 
 
 function App() {
- const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState('');
+  const [visibility, setVisibility] = useState('');
+  const [weather, setWeather] = useState('');
+  const [comment, setComment] = useState('');
 
   useEffect(() => {
     diaryService.getAll().then(setDiaries);
-  },[])
+  }, [])
+  
+  const addDiary = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const newDiary = await diaryService.create({ date, visibility, weather, comment });
+    setDiaries(diaries.concat(newDiary));
+    setDate('');
+    setVisibility('');
+    setWeather('');
+    setComment('');    
+  }
 
 
   return (<div>
     <h1>Flight Diaries</h1>
+    <h2>Add New Entry</h2>
+      <form onSubmit={addDiary}>
+        <div>Date <input type="text" value={date} onChange={e => setDate(e.target.value)} /></div>
+        <div>Visibility <input type="text" value={visibility} onChange={e => setVisibility(e.target.value)} /></div>
+        <div>Weather <input type="text" value={weather} onChange={e => setWeather(e.target.value)} /></div>
+        <div>Comment <input type="text" value={comment} onChange={e => setComment(e.target.value)} /></div>
+        <button type="submit">Add</button>
+      </form>
+
+    <h2>Diary Entries</h2>
     {diaries.map(d => (
       <div key={d.id}>
         <h3>{d.date}</h3>
