ecb922e5d42bcd5ba0ad647c803fb6bec1604a7e
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-09-30 05:17:01 +0100

    updated patient type + entry interface

--- patientor-backend/src/types.ts
+++ patientor-backend/src/types.ts
@@ -11,9 +11,15 @@ export interface Patient {
     ssn: string;
     gender: string;
     occupation: string;
+    entries: Entry[];
 }
 
-export type PublicInfo = Omit<Patient, 'ssn'>;
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface Entry{ }
+
+
+
+export type PublicInfo = Omit<Patient, 'ssn' | 'entries'>;
 export type NewPatient = Omit<Patient, 'id'>;
 
 export enum Gender {
