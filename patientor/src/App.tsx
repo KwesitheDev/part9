d2e852e26ccb8f52cbc342970338408a43034173
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-03 11:00:36 +0100

    fetched patient and diagnosis

--- patientor/src/App.tsx
+++ patientor/src/App.tsx
@@ -1,28 +1,39 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import axios from "axios";
 import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-import { Button, Divider, Container, Typography } from '@mui/material';
+import { Button, Divider, Container, Typography } from "@mui/material";
 
 import { apiBaseUrl } from "./constants";
-import { Patient } from "./types";
+import { useStateValue, setPatientList, setDiagnoses } from "./state";
+import { Patient, Diagnosis } from "./types";
 
+import diagnosesService from "./services/diagnoses";
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
 import PatientPage from "./components/PatientPage";
 
 const App = () => {
-  const [patients, setPatients] = useState<Patient[]>([]);
+  const [, dispatch] = useStateValue();
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
 
-    const fetchPatientList = async () => {
-      const patients = await patientService.getAll();
-      setPatients(patients);
+    const fetchData = async () => {
+      try {
+        const patients: Patient[] = await patientService.getAll();
+
+        const diagnoses: Diagnosis[] = await diagnosesService.getAll();
+
+        dispatch(setPatientList(patients));
+        dispatch(setDiagnoses(diagnoses));
+      } catch (error) {
+        console.error("Error fetching initial data", error);
+      }
     };
-    void fetchPatientList();
-  }, []);
-  
+
+    void fetchData();
+  }, [dispatch]);
+
   return (
     <div className="App">
       <Router>
@@ -30,12 +41,15 @@ const App = () => {
           <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
             Patientor
           </Typography>
+
           <Button component={Link} to="/" variant="contained" color="primary">
             Home
           </Button>
+
           <Divider hidden />
+
           <Routes>
-            <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="/" element={<PatientListPage />} />
             <Route path="/patients/:id" element={<PatientPage />} />
           </Routes>
         </Container>
