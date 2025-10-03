49b26994689e7d2260286e13584b2be013f3c76c
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-03 11:01:32 +0100

    wrapped App in state provider

--- patientor/src/main.tsx
+++ patientor/src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-
+import { StateProvider } from './state.tsx';
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />,
+  <StateProvider>
+    <App />
+  </StateProvider>,
 );
