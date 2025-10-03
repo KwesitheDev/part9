ceb01ac7d6296188f8fccced3011e737ab0b25cb
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-10-03 10:45:14 +0100

    added default export

--- patientor/src/services/diagnoses.ts
+++ patientor/src/services/diagnoses.ts
@@ -3,7 +3,11 @@ import { Diagnosis } from "../types";
 import axios from "axios";
 
 //fetch Diagnosis
-export const getAll = async () => {
+ const getAll = async () => {
     const { data } = await axios.get<Diagnosis[]>(`${apiBaseUrl}/diagnoses`);
     return data;
+};
+
+export default {
+    getAll
 };
